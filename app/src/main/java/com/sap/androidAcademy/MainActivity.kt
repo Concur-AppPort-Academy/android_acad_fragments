a04cdc88a2e0d3570eaaf1aefbdd72d74e08520c
Vladimir Valouch <vladimir.valouch@sap.com>
2021-06-18 11:12:13 +0200

    lesson content mini guide

--- app/src/main/java/com/sap/androidAcademy/MainActivity.kt
+++ app/src/main/java/com/sap/androidAcademy/MainActivity.kt
@@ -7,6 +7,18 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
 
 
+    /**
+     * 1. add dependencies
+     *  implementation "androidx.viewpager2:viewpager2:1.0.0"
+     *  implementation 'com.google.android.material:material:1.4.0-rc01'
+     * 2. add tabLayout and pagerview2
+     * 3. create holder for fragment data
+     * 4. create adapter
+     * 5. bind everything together
+     * 6. enjoy running app ;-)
+     *
+     */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

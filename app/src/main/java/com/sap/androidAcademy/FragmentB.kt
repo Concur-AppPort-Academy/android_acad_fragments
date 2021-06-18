6fcb4c6f46a8f9846e672cc040f52cf818bf6507
Vladimir Valouch <vladimir.valouch@sap.com>
2021-06-18 11:35:28 +0200

    merge in latest master

--- app/src/main/java/com/sap/androidAcademy/FragmentB.kt
+++ app/src/main/java/com/sap/androidAcademy/FragmentB.kt
@@ -2,16 +2,13 @@ package com.sap.androidAcademy
 
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.setFragmentResultListener
 
 class FragmentB : LoggingFragment() {
-
-    private val viewModel:FragmentViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,7 +20,10 @@ class FragmentB : LoggingFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<TextView>(R.id.fragment_b_text).text = "I said ${viewModel.retrieveValue()}"
+        setFragmentResultListener(MY_REQUEST_KEY) { _, bundle ->
+            val result = bundle.getString(MY_BUNDLE_KEY, "none")
+            view.findViewById<TextView>(R.id.fragment_b_text).text = "I said $result"
+        }
     }
 
     override fun getLogTag(): String = "Fragment B"
--- app/src/main/java/com/sap/androidAcademy/MainActivity.kt
+++ app/src/main/java/com/sap/androidAcademy/MainActivity.kt
@@ -7,32 +7,8 @@ import android.widget.Button
 class MainActivity : AppCompatActivity() {
 
 
-    /**
-     * 1. add dependencies
-     *  implementation "androidx.viewpager2:viewpager2:1.0.0"
-     *  implementation 'com.google.android.material:material:1.4.0-rc01'
-     * 2. add tabLayout and pagerview2
-     * 3. create holder for fragment data
-     * 4. create adapter
-     * 5. bind everything together
-     * 6. enjoy running app ;-)
-     *
-     */
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        findViewById<Button>(R.id.fragment_a_btn).setOnClickListener {
-            supportFragmentManager.beginTransaction().replace(
-                R.id.fragment_viewport, FragmentA.newInstance()
-            ).addToBackStack(null).commit()
-
-        }
-        findViewById<Button>(R.id.fragment_b_btn).setOnClickListener {
-            supportFragmentManager.beginTransaction().replace(
-                R.id.fragment_viewport, FragmentB.newInstance()
-            ).addToBackStack(null).commit()
-        }
     }
 }
